033335bbfb36b9d7836438265b4734e8f0a45a20
Joseph Ampadu <josephampadu549@gmail.com>
2022-08-16 15:43:11 +0000

    remove re-run while loop. needs bug fixes

--- src/main/java/io/github/daduam/App.java
+++ src/main/java/io/github/daduam/App.java
@@ -15,26 +15,13 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("🔶 Enter the word to test: ");
-            String word = scanner.nextLine();
+        System.out.print("🔶 Enter the word to test: ");
+        String word = scanner.nextLine();
 
-            if (PalindromeChecker.check(word))
-                System.out.println("✅ " + word + " is a palindrome");
-            else
-                System.out.println("❌ " + word + " is a not palindrome");
-
-            System.out.println("\n😢 Do you want to quit palindrome checker?");
-            System.out.println("\t1. Quit palindrome 🚩");
-            System.out.println("\t2. Continue to test another string 🏁");
-            System.out.print("\n🔶 Enter an option number (1 - 2): ");
-            int option = scanner.nextInt();
-
-            if (option == 1) {
-                System.out.println("👋 Quitting palindrome checker...");
-                break;
-            }
-        }
+        if (PalindromeChecker.check(word))
+            System.out.println("✅ " + word + " is a palindrome");
+        else
+            System.out.println("❌ " + word + " is a not palindrome");
 
         scanner.close();
     }
